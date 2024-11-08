e78c6da46fef538e22764478c9c2806a55176c30
Austin Aurelio <austin@delveam.com>
2024-11-08 00:52:41 -0600

    Init commit: create a window

---
+++ src/clj_pong/core.clj
@@ -0,0 +1,24 @@
+(ns clj-pong.core
+  "Clj Pong"
+  (:import [com.raylib Raylib Raylib$Camera2D]
+           [com.raylib Jaylib])
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (Raylib/InitWindow 400 400 "Demo")
+  (loop [should-close (Raylib/WindowShouldClose)
+         camera (Raylib$Camera2D/new)]
+    (if should-close
+      (Raylib/CloseWindow)
+      (do
+        (Raylib/BeginDrawing)
+        (Raylib/ClearBackground Jaylib/RAYWHITE)
+        (Raylib/BeginMode2D camera)
+        (Raylib/DrawGrid 20, 1.0)
+        (Raylib/EndMode2D)
+        (Raylib/DrawText "Hello world!" 190 200 20 Jaylib/VIOLET)
+        (Raylib/DrawFPS 20 20)
+        (Raylib/EndDrawing)
+        (recur (Raylib/WindowShouldClose) camera)))))
---
+++ test/clj_pong/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-pong.core-test
+  (:require [clojure.test :refer :all]
+            [clj-pong.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
