3722dccb59d2c62227e2e9812246bac4dab3e856
Austin Aurelio <austin@delveam.com>
2024-11-08 23:05:01 -0600

    Checkpoint for project restructure
    Moving to libGDX because of crashes...

--- src/clj_pong/core.clj
+++ src/clj_pong/core.clj
@@ -1,25 +1,63 @@
 (ns clj-pong.core
   "Clj Pong"
   (:import [com.raylib Raylib Raylib$Camera2D]
-           [com.raylib Jaylib])
+           [com.raylib Jaylib Jaylib$Vector2])
   (:gen-class))
+
+(defn tick
+  [state]
+  (let [new-entities (mapv #(or ((:update-fn %) %) %) (:entities state))]
+    (assoc state :entities new-entities)))
+
+(defn draw
+  [state]
+  (Raylib/BeginDrawing)
+  (Raylib/ClearBackground Jaylib/RAYWHITE)
+  (Raylib/BeginMode2D (:camera state))
+  (Raylib/DrawCircle 0 0 100.0 Jaylib/GREEN)
+  (Raylib/EndMode2D)
+  (Raylib/DrawText "Hello" 190 200 20, Jaylib/VIOLET)
+  (Raylib/DrawFPS 20 20)
+  (Raylib/EndDrawing)
+  state)
+
+(def init-entities
+  [{:name "Ball"
+    :velocity-x 0.0
+    :velocity-y 0.0
+    :x 0
+    :y 0
+    :update-fn #(-> %
+                    (assoc :x (+ (:x %) (:velocity-x %)))
+                    (assoc :y (+ (:y %) (:velocity-y %))))
+    :draw-fn #()}]);Raylib/DrawCircle 0 0 100.0 Jaylib/VIOLET)}]) ;(:x %) (:y %) 500.0 Jaylib/VIOLET)}])
+
+(def init-state {:entities init-entities
+                 :camera (doto
+                           (Raylib$Camera2D/new)
+                           (.offset (Jaylib$Vector2/new 200.0 200.0))
+                           (.zoom 1.0))})
+
+(defn start-game
+  []
+  (Raylib/InitWindow 400 400 "Demo")
+  (Raylib/SetTargetFPS 60)
+  (loop [should-close (Raylib/WindowShouldClose)
+         state init-state]
+    (if should-close
+      (Raylib/CloseWindow)
+      (->> state
+          (tick)
+          (draw)
+          (recur (Raylib/WindowShouldClose))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (Raylib/InitWindow 400 400 "Demo")
-  (Raylib/SetTargetFPS 60)
-  (loop [should-close (Raylib/WindowShouldClose)
-         camera (Raylib$Camera2D/new)]
-    (if should-close
-      (Raylib/CloseWindow)
-      (do
-        (Raylib/BeginDrawing)
-        (Raylib/ClearBackground Jaylib/RAYWHITE)
-        (Raylib/BeginMode2D camera)
-        (Raylib/DrawGrid 20 1.0)
-        (Raylib/EndMode2D)
-        (Raylib/DrawText "Hello world!" 190 200 20 Jaylib/VIOLET)
-        (Raylib/DrawFPS 20 20)
-        (Raylib/EndDrawing)
-        (recur (Raylib/WindowShouldClose) camera)))))
+  (future (start-game)))
+
+(comment
+  (-main)
+  (Raylib/SetTargetFPS 120)
+  (:camera init-state))
+
